e93bdf458dc197c3dec00b2432919d2f89970ca3
Администратор <1@1.ua>
2016-03-25 13:09:19 +0200

    today

--- app/src/main/java/com/mihail/mycam/MainActivity.java
+++ app/src/main/java/com/mihail/mycam/MainActivity.java
@@ -1,24 +1,47 @@
 package com.mihail.mycam;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.style.UpdateLayout;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private EditText mCatName;
-    private TextView mHelloTextView;
+    ListView listItemView;
+
+    // Define string array.
+    String[] listItemsValue = new String[] {"Android","PHP","Web Development","Blogger","SEO","Photoshop", "1", "2", "3", "4", "5", "6"};
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mHelloTextView = (TextView)findViewById(R.id.textView);
-        mCatName = (EditText)findViewById(R.id.editText);
+
+        listItemView = (ListView)findViewById(R.id.listView1);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2, android.R.id.text1, listItemsValue);
+
+        listItemView.setAdapter(adapter);
+
+        // ListView setOnItemClickListener function apply here.
+
+        listItemView.setOnItemClickListener(new OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                listItemsValue[position] = "New!!";
+                adapter.notifyDataSetChanged();
+                //Toast.makeText(MainActivity.this, listItemsValue[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-    public void OnClick(View view) {
-        mHelloTextView.setText(getString(R.string.Hello) + mCatName.getText());
-    }
+
 }
